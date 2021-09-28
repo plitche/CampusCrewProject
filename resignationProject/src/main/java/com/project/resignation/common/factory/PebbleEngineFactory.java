b410c39177eeef3c3c30c17513ac4080c5333f9d
DESKTOP-8JMNCLE\wjdgh <wjdgh823@naver.com>
2021-09-28 18:32:48 +0900

    pebble 설정 추가

--- resignationProject/src/main/java/com/project/resignation/common/factory/PebbleEngineFactory.java
+++ resignationProject/src/main/java/com/project/resignation/common/factory/PebbleEngineFactory.java
@@ -3,14 +3,15 @@ package com.project.resignation.common.factory;
 import com.mitchellbosecke.pebble.PebbleEngine;
 import com.mitchellbosecke.pebble.loader.Loader;
 import com.mitchellbosecke.pebble.spring4.PebbleViewResolver;
+import com.mitchellbosecke.pebble.spring4.extension.SpringExtension;
 import com.project.resignation.common.template.TemplateViewExtension;
 
 public class PebbleEngineFactory {
-	public static PebbleEngine instance(Loader<?> loader, TemplateViewExtension templateExtension, boolean cacheActive) {
+	public static PebbleEngine instance(Loader<?> loader, SpringExtension springExtension, boolean cacheActive) {
 		
 		return new PebbleEngine.Builder()
 				.loader(loader)
-				.extension(templateExtension)
+				.extension(springExtension)
 				.cacheActive(cacheActive)
 				.build();
 	}
