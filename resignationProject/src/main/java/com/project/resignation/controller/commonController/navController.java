5311a4834c5b50a7f73479215e92df34f5f27b74
plitche <kysu728@naver.com>
2021-10-07 00:57:21 +0900

    공통 js 수정 및 controller 제어

---
+++ resignationProject/src/main/java/com/project/resignation/controller/commonController/navController.java
@@ -0,0 +1,27 @@
+package com.project.resignation.controller.commonController;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.project.resignation.service.TestService;
+
+@Controller
+@RequestMapping(value="/navigation")
+public class navController {
+
+	@Autowired
+	TestService testService; 
+	
+	@RequestMapping(value="/{navigationBtn}", method=RequestMethod.GET)
+	public String goIndex(@PathVariable("navigationBtn") String navigationBtn,
+									Model model) throws Exception {
+		
+		
+		return navigationBtn;
+	}
+	
+}
--- resignationProject/src/main/webapp/resources/js/common/main.js
+++ resignationProject/src/main/webapp/resources/js/common/main.js
@@ -1,8 +1,25 @@
 var test = (function() {
 	var that = {
-			test2: function() {
-				alert('123');
-			}			
+			Init : function() {
+				// 상단 nav 메뉴 세팅
+				that.Event.navBtnEvent();
+				
+			},
+			
+			Event :  {
+				navBtnEvent : function() {
+					// 홈 버튼
+					$("#homeBtn").on("click", function() {
+						location.href="/resignation";
+					});
+					
+					$('#headerNav a').on('click', function() {
+						location.href= "/resignation/navigation/" + $(this).attr('btnName');
+					});
+				}
+				
+			}
+			
 	}
 	return that;
 }) ();;
