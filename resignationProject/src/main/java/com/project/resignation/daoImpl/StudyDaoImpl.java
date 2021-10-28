e6b725da5a66dc4cc5a0191abc79f5b673850282
plitche <kysu728@naver.com>
2021-10-29 01:23:14 +0900

    study list 불러오기 데이트 포맷 바꾸기

--- resignationProject/src/main/java/com/project/resignation/daoImpl/StudyDaoImpl.java
+++ resignationProject/src/main/java/com/project/resignation/daoImpl/StudyDaoImpl.java
@@ -1,5 +1,8 @@
 package com.project.resignation.daoImpl;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -17,6 +20,17 @@ public class StudyDaoImpl implements StudyDao {
 	
 	@Override
 	public List<Map<String, Object>> goStudyMain() {
-		return sqlsession.selectList("Study.getStudyInfoList");
+		List<Map<String, Object>> studyList = sqlsession.selectList("Study.getStudyInfoList");
+		
+		for (Map<String, Object> studyItem : studyList) {
+			// DB에서 조회한 date타입의 날짜를 2021. 10. 29 형태로 formatting
+			SimpleDateFormat changeDate = new SimpleDateFormat("yyyy. MM. dd");
+			String tempStartDate = changeDate.format(studyItem.get("DTSTARTDATE"));
+			String tempEndDate = changeDate.format(studyItem.get("DTSTARTDATE"));
+			studyItem.replace("DTSTARTDATE", tempStartDate);
+			studyItem.replace("DTENDDATE", tempEndDate);
+		}
+		
+		return studyList;
 	}
 }
