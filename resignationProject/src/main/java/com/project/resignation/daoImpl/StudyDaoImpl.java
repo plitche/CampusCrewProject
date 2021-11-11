6a7f0f099a2e8f6e97eb37a364943c54d57a04c5
plitche <kysu728@naver.com>
2021-11-12 01:30:55 +0900

    날짜 format , try-catch, 날짜 계산 등 완료

--- resignationProject/src/main/java/com/project/resignation/daoImpl/StudyDaoImpl.java
+++ resignationProject/src/main/java/com/project/resignation/daoImpl/StudyDaoImpl.java
@@ -2,6 +2,7 @@ package com.project.resignation.daoImpl;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -24,7 +25,7 @@ public class StudyDaoImpl implements StudyDao {
 		
 		for (Map<String, Object> studyItem : studyList) {
 			// DB에서 조회한 date타입의 날짜를 2021. 10. 29 형태로 formatting
-			SimpleDateFormat changeDate = new SimpleDateFormat("yyyy. MM. dd");
+			SimpleDateFormat changeDate = new SimpleDateFormat("yyyy.mm.dd");
 			String tempStartDate = changeDate.format(studyItem.get("DTSTARTDATE"));
 			String tempEndDate = changeDate.format(studyItem.get("DTSTARTDATE"));
 			studyItem.replace("DTSTARTDATE", tempStartDate);
@@ -39,18 +40,25 @@ public class StudyDaoImpl implements StudyDao {
 		Map<String, Object> eachStudyInfo = sqlsession.selectOne("Study.getEachStudyInfo", iStudyNo);
 		
 		// todo 게시글 insert시 hit 0으로 insert 시키기
-		// DB에서 조회한 date타입의 날짜를 2021. 10. 29 형태로 formatting
-		SimpleDateFormat changeDate = new SimpleDateFormat("yyyy. MM. dd");
-		String tempStartDate = changeDate.format(eachStudyInfo.get("DTSTARTDATE"));
-		String tempEndDate = changeDate.format(eachStudyInfo.get("DTENDDATE"));
-		eachStudyInfo.replace("DTSTARTDATE", tempStartDate);
-		eachStudyInfo.replace("DTENDDATE", tempEndDate);
-
-		// todo 자바 날짜 계산
-//		SimpleDateFormat format = new SimpleDateFormat("yyyy-mm-dd");
-//		long duration = format.parse(tempStartDate).getTime() - format.parse(tempEndDate).getTime();
-//		eachStudyInfo.put("duration", duration);
-//		System.out.println(duration);
+		
+		try {
+			SimpleDateFormat changeDate = new SimpleDateFormat("yyyy-mm-dd");
+			
+			// 자바 날짜 계산
+	        // 두 날짜를 parse()를 통해 Date형으로 변환.
+	        // Date로 변환된 두 날짜를 계산한 뒤 그 리턴값으로 long type 변수를 초기화 하고 있다.
+	        // Date.getTime() 은 해당날짜를 기준으로1970년 00:00:00 부터 몇 초가 흘렀는지를 반환해준다.
+			String tempStartDate = changeDate.format(eachStudyInfo.get("DTSTARTDATE"));
+			String tempEndDate = changeDate.format(eachStudyInfo.get("DTENDDATE"));
+			eachStudyInfo.replace("DTSTARTDATE", tempStartDate);
+			eachStudyInfo.replace("DTENDDATE", tempEndDate);
+			long duration = changeDate.parse(tempStartDate).getTime() - changeDate.parse(tempEndDate).getTime();
+			
+			eachStudyInfo.put("duration", duration);	
+		} catch (Exception e) {
+			e.printStackTrace();
+			eachStudyInfo.put("duration", "-");
+		}
 		
 		return eachStudyInfo;
 	}
