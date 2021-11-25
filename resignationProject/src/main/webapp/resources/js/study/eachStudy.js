123b193ce248b08a514058f2c087a9eb32250b4f
plitche <kysu728@naver.com>
2021-11-26 00:04:52 +0900

    스터디 메인 페이지 스와이퍼 설정 스터디 상세 페이지 포지션 지원하기 기능 js로 변경 (input hidden 이외에 데이터 js파일로 넘길 방법 생각하기)

--- resignationProject/src/main/webapp/resources/js/study/eachStudy.js
+++ resignationProject/src/main/webapp/resources/js/study/eachStudy.js
@@ -1,6 +1,9 @@
 var study = (function() {
 	var that = {
 			init : function() {
+
+				// 포지션 지원 Event
+				that.Event.applyPosition();
 				
 				// 카카오 URL 공유 세팅
 				that.Event.kakaoInit();
@@ -8,11 +11,28 @@ var study = (function() {
 				// 마우스 오버 이벤트
 				that.Event.writerMouseOver();
 				
-				
 			},
 			
 			Ajax : {
-					
+				// 포지션 지원 ajax
+				applyPositionAjax : function(postData) {
+					requestAjaxPOST('/resignation/study/applyPosition', postData, function(data) {
+						switch(data.iReturn) {
+						case 0 : 
+							msg.info('포지션 지원에 성공하셨습니다.')
+							break;
+						case 1 : 
+							msg.info('해당 포지션은 더이상 남은 자리가 없습니다.')
+							break;
+						case 2 : 
+							msg.info('이미 해당 스터디의 [' + data.appliedPosition + ']포지션에 신청 했었습니다. \n다시 신청 하고 싶으시면 마이페이지에서 취소뒤 다시 시도해주세요.')						
+							break;
+						default : 
+							msg.info('오류가 발생하였습니다. 나정호씨에게 문의해주세요.')
+							break;
+						}
+					});
+				}
 			},
 			
 			Template : {
@@ -20,6 +40,19 @@ var study = (function() {
 			},
 			
 			Event :  {
+				// 포지션 지원 Event
+				applyPosition : function() {
+					$('.positionApplyBtn').click(function() {
+						var positionName = $(this).siblings('.positionTxt').text();
+						var studyNum = $('#studyNum').val();
+
+						var postData = {'iStudyNo' : studyNum, 
+											   'positionName' : positionName, 
+											   'applyID' : 'plitche'};
+						
+						that.Ajax.applyPositionAjax(postData);
+					});
+				},
 				// 카카오톡 공유하기 btn event
 				// todo 로그인 체크
 				kakaoInit : function() {
