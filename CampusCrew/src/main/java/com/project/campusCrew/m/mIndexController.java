fbe407669f4f847d0dc5b895adba6255e16ea3e2
DESKTOP-8JMNCLE\wjdgh <wjdgh823@naver.com>
2021-12-29 00:52:10 +0900

    mobile file commit

--- CampusCrew/src/main/java/com/project/campusCrew/m/mIndexController.java
+++ CampusCrew/src/main/java/com/project/campusCrew/m/mIndexController.java
@@ -1,4 +1,4 @@
-package com.project.campusCrew.test;
+package com.project.campusCrew.m;
 
 import java.text.DateFormat;
 import java.util.Date;
@@ -6,43 +6,28 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.project.campusCrew.test.service.TestService;
-
-/**
- * Handles requests for the application home page.
- */
 @Controller
-public class HomeController {
+public class mIndexController {
+
+	private static final Logger logger = LoggerFactory.getLogger(mIndexController.class);
 	
-	@Autowired
-	TestService testService; 
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 * @throws Exception 
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping(value = "/m", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) throws Exception {
-		logger.info("Welcome home! The client locale is {}.", locale);
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
 		String formattedDate = dateFormat.format(date);
 		
-		model.addAttribute("test", testService.testServiceMethod1());
-		
 		model.addAttribute("serverTime", formattedDate );
 		System.out.println("잘하자");
-		return "index";
+		return "m/index";
 	}
+	
 	
 }
