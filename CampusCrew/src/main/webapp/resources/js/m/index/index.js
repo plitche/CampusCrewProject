b3db9076b44e66eecfb6d2adb4ac141096a94c39
DESKTOP-8JMNCLE\wjdgh <wjdgh823@naver.com>
2022-01-01 01:32:31 +0900

    개발 팝업환경 세팅중

--- CampusCrew/src/main/webapp/resources/js/m/index/index.js
+++ CampusCrew/src/main/webapp/resources/js/m/index/index.js
@@ -1,8 +1,8 @@
-/*
- * 
+/**
  * 
  * 
  */
+
 
 // 로딩되고 실행할 이벤트
 $(function(){
@@ -12,4 +12,20 @@ $(function(){
 		$('.crewRecruit_list').toggle();
 	});
 	
+	$('.crewRecruit_thunder').off('click').on('click', function() {
+		console.log('번개크루 팝업화면 열자!');
+		$('#thunderStep01').addClass('layer_open');
+	});
+
+	$('.crewRecruit_regular').off('click').on('click', function() {
+		console.log('정기크루 팝업화면 열자!');
+	});
+
+	$('.crewRecruit_battle').off('click').on('click', function() {
+		console.log('배틀크루 팝업화면 열자!');
+	});
+	
+	
 });
+
+
