aa20b65871bcff4f0cadb9b1c18dfa6e3175b326
plitche <kysu728@naver.com>
2022-01-03 02:06:39 +0900

    battle 기초 세팅 작업 시작

---
+++ CampusCrew/src/main/java/com/project/campusCrew/m/battle/manager/managerBattleController.java
@@ -0,0 +1,25 @@
+package com.project.campusCrew.m.battle.manager;
+
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+// 모바일 indexController
+@Controller
+@RequestMapping(value="/m/managerBattle")
+public class managerBattleController {
+
+    private static final Logger logger = LoggerFactory.getLogger(managerBattleController.class);
+
+    @RequestMapping(value = "/main", method = RequestMethod.GET)
+    public String crewBattleMain(Locale locale, Model model) throws Exception {
+        
+        return "m/home/battle/crew/crewBattleMain";
+    }
+    
+}
---
+++ CampusCrew/src/main/webapp/resources/js/m/home/battle/manager/managerBattleMain.js
@@ -0,0 +1,20 @@
+let crewBattleMain = (function() {
+	let that = {
+			init : function() {
+
+			},
+			
+			Ajax : {
+				
+			},
+			
+			Template : {
+				
+			},
+			
+			Event :  {
+				
+			},
+	}
+	return that;
+}) ();;
